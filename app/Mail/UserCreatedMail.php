f22d2a8101e19f1670b983b31de439746c1372ce
Hardy Prabowo <hardy.prabowo@napinfo.co.id>
2025-11-12 10:18:15 +0700

    create email template

---
+++ app/Mail/UserCreatedMail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UserCreatedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $user;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('Thanks for Join Our System')
+                    ->view('emails.user_created');
+    }
+}

f399b467ceab098a782dc8dfa697fc385d0f0708
Hardy Prabowo <hardy.prabowo@napinfo.co.id>
2025-11-12 10:05:43 +0700

    migrate table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', ['administrator', 'manager', 'user'])->default('user');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
